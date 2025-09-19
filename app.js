3ac8608b8aa5a1cb40cd3ac25ad234fb2813eb94
JThomas333 <98952709+JThomas333@users.noreply.github.com>
2025-09-19 14:10:39 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -11,3 +11,61 @@ let customers = [
     {id: 3, name: "Józsi", email: "test.jo@gmail.com"},
 
 ];
+
+
+
+app.get("/customers", (req, res) => {
+  res.status(200).json(customers)
+});
+
+app.get("/customers/:id", (req, res) => {
+
+const id = req.params.id
+    const [customer] = customers.filter(customer => customer.id == id)
+    if(!customer){
+      return  res.status(404).json({messemail: "customer not found" })
+    }
+    res.status(200).json(customer)
+    });
+
+    
+app.post("/customers", (req, res) => {
+    const {name, email} = req.body
+    if(!name || !email){
+        res.status(400).json({messemail: "Invalid credentials" })
+    }
+    const id = customers[customers.length-1]?.id + 1 || 1
+    const customer = {id, name, email}
+    customers.push(customer)
+    res.status(201).json({messemail:"Succesful upload"})
+  
+})
+
+app.put("/customers/:id", (req, res) => {
+     const id = +req.params.id
+    const [customer] = customers.find(customer => customer.id == id)
+    if(!customer){
+      return  res.status(404).json({messemail: "customer not found" })
+    }
+    const {name, email} = req.body
+    if(!name || !email){
+        res.status(400).json({messemail: "Invalid credentials" })
+    }
+    const index = customers.indexOf(customer);
+    customers[index] = {id, name, email}
+
+    res.status(200).json(customers[index])
+});
+
+app.delete("/customers/:id", (req, res) => {
+
+    const id = +req.params.id;
+    customers = customers.filter((customer) => customer.id != id)
+    res.status(200).json({messemail: "Deleted successfully"})
+});
+
+
+app.listen(PORT, () => console.log(`App runs on http://localhost:${PORT} `));
+
+
+
